7e9ab4af65369ca7653c184f75724583c01a17bf
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-08-17 17:42:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){}
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    canvas.background("white");
+}
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("white");
+    console.log("Background changed to white")
+}
